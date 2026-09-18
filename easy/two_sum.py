3595c15028a6d83551a7be3e031c82be95c29257
Nathan Woo <nathanwoo22@gmail.com>
2026-09-17 21:54:13 -0600

    two_sum

---
+++ easy/two_sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        complements = {}
+        for i in range(0, len(nums)):
+            complements[target - nums[i]] = i
+
+        for i in range(0, len(nums)):
+            if nums[i] in complements and i != complements[nums[i]]:
+                return [i, complements[nums[i]]]
